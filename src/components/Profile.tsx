fd9354e3a725f8ff036b87ed0631fc13773fb9c6
Kemuel Batista <kemuellima20@gmail.com>
2021-02-27 15:02:22 -0300

    Adding a new content

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 
 import styles from '../styles/components/Profile.module.css';
 
 export default function Profile(){
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/kemuel-batista.png" alt="Kemuel Batista"/>
@@ -11,7 +14,7 @@ export default function Profile(){
         <strong>Kemuel Batista</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>

d7b443371ad474728113fe620553f03fb5f38369
Kemuel Batista <kemuellima20@gmail.com>
2021-03-02 16:41:02 -0300

    suceeded

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 export function CountdownProvider({ children }: CountdownProviderProps){
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.05 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -46,7 +46,7 @@ export function CountdownProvider({ children }: CountdownProviderProps){
 
   function resetCountdown() {
     clearTimeout(countdownTimeout);
-    setTime(0.05 * 60);
+    setTime(25 * 60);
     setIsActive(false);
     setHasFinished(false);
   }
